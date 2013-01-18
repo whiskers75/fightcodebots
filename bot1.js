fe3582e17c2bcd5619a3b4f4ec4ef7fb6755d2c2
whiskers75 <otto.heese@me.com>
2013-01-18 19:28:37 +0000

    Update bot1.js

--- bot1.js
+++ bot1.js
@@ -4,41 +4,35 @@
 //Forks of this may not battle this robot.
 //With whiskers75's Lock-On Tech (c)
 
-var cannonStick = false;
 var lastSighting = 60;
 var Robot = function(robot) {
   robot.log("whiskers75's robot initialized");
-
+  robot.log("Cur health: " + ev.life);
+  robot.ahead(12);
+  robot.rotateCannon(2);
 };
 
 Robot.prototype.onHitByBullet = function(ev) {
     var robot;
     robot = ev.robot;
-    robot.turn(90 - ev.bulletBearing);
+    robot.turn(90 + ev.bulletBearing);
     robot.rotateCannon(90 - ev.bulletBearing);
-    robot.fire();
-  robot.fire();
-  robot.fire();
-  robot.ahead(100)
-  robot.rotateCannon(180)
-    cannonStick = true;
+    robot.ahead(100);
+    robot.rotateCannon(180);
+    robot.log("Hit! Health: " + ev.life);
 };
 Robot.prototype.onIdle = function(ev) {
     var robot = ev.robot;
-    robot.rotateCannon(lastSighting / 6);
-  robot.ahead(5);
-
-   robot.log("Idle...");
-  
+    robot.rotateCannon(lastSighting / 1.212121212);
+    robot.ahead(5);
 };
 
 Robot.prototype.onScannedRobot = function(ev) {
     var robot = ev.robot;
     robot.fire();
-  robot.log("Found Robot");
-  robot.rotateCannon(lastSighting / 6 - lastSighting /6 * 3.15);
-};
-Robot.prototype.onWallCollision = function(ev) {
-    var robot = ev.robot; // turn enought to be in a straight
-                            // angle with the wall.
+    robot.log("Locked on to Robot ID "+ ev.scannedRobot.id);
+    robot.rotateCannon(lastSighting / 6 - lastSighting /6 * 3.1);
+    robot.rotateCannon(lastSighting / 6 - lastSighting /6 / 3.1);
+    robot.rotateCannon(lastSighting / 6 - lastSighting /6 * 3.1);
+    robot.rotateCannon(lastSighting / 6 - lastSighting /6 / 3.1);
 };
